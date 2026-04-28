2b0a9cbd322604773bacee6d16f4b16b7668da00
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2026-04-27 21:54:44 -0600

    ADDITION: Automatically create wrapper resource
    Creating a GUIDEMappingContext or a GUIDEAction will now automatically create a CS wrapper and assign the GUIDE resource to the wrapper.

--- addons/guideCS/Plugin.cs
+++ addons/guideCS/Plugin.cs
@@ -1,5 +1,6 @@
 #if TOOLS
 using Godot;
+using Godot.Collections;
 
 namespace GuideCs;
 
@@ -33,7 +34,52 @@ public partial class Plugin : EditorPlugin
         var cfgLoader = new ConfigFile();
         cfgLoader.Load($"{PluginPath}/plugin.cfg");
 
+        ResourceSaved += OnResourceSaved;
+
         base._EnterTree();
+    }
+
+    private void OnResourceSaved(Resource resource)
+    {
+        Script resourceScript = (Script)resource.GetScript();
+        string savePath = resource.ResourcePath.TrimSuffix(".tres") + "CS.tres";
+
+        if (GetClassName(resourceScript) == "GUIDEMappingContext")
+        {
+            GuideMappingContext newCSmap = new();
+            newCSmap.ConnectBaseGuideResource(resource);
+            CreateCSResource(newCSmap, savePath);
+        }
+        else if (GetClassName(resourceScript) == "GUIDEAction")
+        {
+            GuideAction newCSAction = new();
+            newCSAction.ConnectBaseGuideResource(resource);
+            CreateCSResource(newCSAction, savePath);
+        }
+    }
+
+    private void CreateCSResource(Resource resource, string path)
+    {
+        Error error = ResourceSaver.Save(resource, path);
+
+        if (error > 0)
+        {
+            GD.PushWarning("guideCS was unable to create the CS wrapper automatically.");
+        }
+        GD.Print($"guideCS created {path} and assigned the GUIDE resource to the wrapper.");
+
+    }
+    
+    private string GetClassName(Script script)
+    {
+        foreach (Dictionary info in ProjectSettings.GetGlobalClassList())
+        {
+            if ((string)info["path"] == script.ResourcePath)
+            {
+                return (string)info["class"];
+            }
+        }
+        return script.GetClass();
     }
 }
 #endif
