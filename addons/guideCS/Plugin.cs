c59b59efba7e74c4eacf6d3c976f750f677191c4
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2026-04-27 18:31:34 -0600

    REMOVE: Verification
    Verification removed, with guide being bundled with the addon the verification was unnessessary.

--- addons/guideCS/Plugin.cs
+++ addons/guideCS/Plugin.cs
@@ -31,73 +31,9 @@ public partial class Plugin : EditorPlugin
         editorInterface = (EditorInterface)Engine.GetSingleton("EditorInterface");
 
         var cfgLoader = new ConfigFile();
-        if (cfgLoader.Load($"{PluginPath}/plugin.cfg") is Error.Ok)
-        {
-            if (cfgLoader.GetValue("plugin", "suppressCheck", "false").AsString() != "true")
-            {
-                _csCfg = cfgLoader;
-                CheckVersionCompatibility();
-            }
-        }
-        
+        cfgLoader.Load($"{PluginPath}/plugin.cfg");
+
         base._EnterTree();
-    }
-    
-    private void CheckVersionCompatibility()
-    {
-        string displayText;
-
-        if (!FileAccess.FileExists(ResourceLibrary.PluginCfgPath))
-        {
-            displayText = $"Unable to verify GuideCs wrapper compatibility: cannot find G.U.I.D.E. config file. Ensure G.U.I.D.E. is installed and the install directory is correct in the '{nameof(ResourceLibrary)}.cs' file.";
-            LoadDialogueBox();
-            return;
-        }
-
-        var gdCfg = new ConfigFile();
-        if (gdCfg.Load(ResourceLibrary.PluginCfgPath) != Error.Ok)
-        { return; }
-        
-        var csVersion = _csCfg.GetValue("plugin", "version", "").AsString();
-        var gdVersion = gdCfg.GetValue("plugin", "version", "").AsString();
-        if (gdVersion == "" || csVersion == "")
-        {
-            displayText = "Unable to verify GuideCs wrapper compatibility with G.U.I.D.E.: cannot parse version values in one or both config files.";
-            LoadDialogueBox();
-            return;
-        }
-            
-        if (gdVersion != csVersion)
-        {
-            displayText = $"GuideCs version [{csVersion}] should match G.U.I.D.E. version [{gdVersion}] but found mismatch. Recommend matching versions to ensure highest compatibility.";
-            LoadDialogueBox();
-        }
-
-        return;
-        void LoadDialogueBox()
-        {
-            _scene = ResourceLoader.Load<PackedScene>($"{PluginPath}/CsTools/VerificationScene.tscn").Instantiate<CanvasLayer>();
-            if (_scene is null)
-            { return; }
-
-            _scene.GetNode<Button>("%Button").Pressed += OnVerifyOkPressed;
-            _scene.GetNode<Label>("%Label").Text = displayText;
-            EditorInterface.Singleton.GetBaseControl().AddChild(_scene);
-        }
-    }
-
-    private void OnVerifyOkPressed()
-    {
-        var suppress = _scene.GetNode<CheckBox>("%CheckBox").ButtonPressed;
-        if (suppress)
-        {
-            _csCfg.SetValue("plugin", "suppressCheck", "true");
-            _csCfg.Save($"{PluginPath}/plugin.cfg");
-        }
-
-        _scene.Visible = false;
-        _scene.GetNode<Button>("%Button").Pressed -= OnVerifyOkPressed;
-        _scene.QueueFree();
     }
 }
 #endif
